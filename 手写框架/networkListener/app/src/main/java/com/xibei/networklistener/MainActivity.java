e7dd790e72da6a4dba182eaaae55f0401e90cac4
jiezongnewstar <ibossjia@gmail.com>
2019-05-23 17:17:00 +0800

    常规架构型网络监听

--- 手写框架/networkListener/app/src/main/java/com/xibei/networklistener/MainActivity.java
+++ 手写框架/networkListener/app/src/main/java/com/xibei/networklistener/MainActivity.java
@@ -1,13 +1,35 @@
 package com.xibei.networklistener;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 
-public class MainActivity extends AppCompatActivity {
+import com.xibei.networklibrary.NetChangeObserver;
+import com.xibei.networklibrary.NetState;
+import com.xibei.networklibrary.NetworkManager;
+
+import static com.xibei.networklibrary.Constants.NETWORK_TAG;
+
+public class MainActivity extends Activity implements NetChangeObserver {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //注册监听
+        NetworkManager.getInstance().setObserver(this);
+
+    }
+
+    @Override
+    public void onConnect(NetState state) {
+        Log.e(NETWORK_TAG,MainActivity.this.getClass().getName()+"网络连接"+state);
+    }
+
+    @Override
+    public void disConnect() {
+        Log.e(NETWORK_TAG,MainActivity.this.getClass().getName()+"网络连接断开");
+
     }
 }
--- 手写框架/networkListener/app/src/main/java/com/xibei/networklistener/MyApplicaiton.java
+++ 手写框架/networkListener/app/src/main/java/com/xibei/networklistener/MyApplicaiton.java
@@ -1,10 +1,20 @@
 package com.xibei.networklistener;
 
+import android.app.Application;
+
+import com.xibei.networklibrary.NetworkManager;
+
 /**
  * Created by Xibei on 2019/5/23.
  * Github: https://github.com/jiezongnewstar
  * Email: ibossjia@gmail.com
  * Deeclaration:
  */
-public class MyApplicaiton {
+public class MyApplicaiton extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        NetworkManager.getInstance().init(this);  //广播注册兼容
+    }
 }
